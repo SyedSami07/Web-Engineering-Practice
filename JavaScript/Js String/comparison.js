cae5bcabb7da223de46e7b7b14570041210f76bf
Syed Salman Sami <syedsamigithub@gmail.com>
2026-07-24 15:52:13 +0600

    lower case, upper case, trim method

---
+++ JavaScript/Js String/comparison.js
@@ -0,0 +1,43 @@
+const subject = 'chemistry';
+const book  = 'CheMiStry';
+
+// console.log(book.toLowerCase());
+
+// if(subject === book) {
+//     console.log('poira poira fatai felbo');
+// }
+// else {
+//     console.log('Eibar ki auto pass ase?');
+// }
+
+
+// if(subject.toLowerCase() === book.toLowerCase()) {
+//     console.log('poira poira fatai felbo');
+// }
+// else {
+//     console.log('Eibar ki auto pass ase?');
+// }
+
+
+const emailUserProvided = 'sami@gmail.com ';
+const emailSavedInDatabase = 'sami@gmail.com';
+
+
+if(emailUserProvided.trim() === emailSavedInDatabase.trim()){
+    console.log('welcome to the website');
+}
+
+else{
+    console.log('duroi giya mor');
+}
+
+
+// if(emailSavedInDatabase.toUpperCase() === emailSavedInDatabase.toUpperCase()){
+//     console.log('miligese!!');
+// }
+
+// else{
+//     console.log('milse na re ba');
+// }
+
+
