99793c6d37147156179cba6d95bd0a66120a4bed
Syed Salman Sami <syedsamigithub@gmail.com>
2026-07-24 16:19:26 +0600

    Add files via upload

---
+++ JavaScript/Js String/includes.js
@@ -0,0 +1,13 @@
+const email = 'sami@gmail.com'
+
+if(email.includes('.')){
+    console.log('has dot')
+}
+
+const fileName = 'my final Final final khudar kosom final.png'
+
+const twitterHandle = '@ElonMusk'
+console.log(fileName.endsWith('.png'))
+console.log(fileName.endsWith('.pdf'))
+console.log(twitterHandle.startsWith('@'))
+
