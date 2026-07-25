9a317a29d388a86118dfd8710f0c90da91693991
Syed Salman Sami <syedsamigithub@gmail.com>
2026-07-25 22:17:14 +0600

    Add files via upload

---
+++ JavaScript/Js Functions/function.js
@@ -0,0 +1,15 @@
+function sayHello(name){
+    console.log('hello!', name)
+
+}
+sayHello('sallu')
+
+function add(num1, num2){
+    console.log('you have provided ', num1,num2)
+    const sum = num1 + num2
+    console.log(sum)
+}
+
+add(3,4)
+
+
