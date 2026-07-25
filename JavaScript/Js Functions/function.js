77cb8758694878b5c80517963cb3b87c6a404e9b
Syed Salman Sami <syedsamigithub@gmail.com>
2026-07-25 22:22:01 +0600

    Add files via upload

--- JavaScript/Js Functions/function.js
+++ JavaScript/Js Functions/function.js
@@ -9,7 +9,22 @@ function add(num1, num2){
     const sum = num1 + num2
     console.log(sum)
 }
-
 add(3,4)
+
+
+function multiply(a, b){
+    const results = a * b;
+    console.log("result of multiplication is: ", results)
+}
+
+multiply(16,2)
+
+
+function square ( number ){
+    const result = number * number;
+    console.log('square is: ', result)
+}
+
+square(5)
 
 
