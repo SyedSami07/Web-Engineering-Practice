c119f711cfe8f06e1dc238c615e33f59029a81ed
Syed Salman Sami <syedsamigithub@gmail.com>
2026-07-29 01:09:21 +0600

    Implement function to find smallest element in array
    Added a function to find the smallest element in an array and log the current element during iteration.

--- Js Problem Solving/problem 10.js
+++ Js Problem Solving/problem 10.js
@@ -1,1 +1,17 @@
 
+// find the smallest number in an array
+
+// with function
+
+function findSmallestElement(arr){
+let smallest = arr[0];
+    for(let i = 0; i<arr.length; i++){
+        let currentElement = arr[i];
+        if(currentElement < smallest){
+            smallest = currentElement;
+        }
+       console.log("Array is: ", currentElement)
+    } return smallest;
+}
+
+console.log("Smallest Element is: ",findSmallestElement([6,33,234,677,800]))
