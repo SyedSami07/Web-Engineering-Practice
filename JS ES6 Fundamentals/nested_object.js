f742cc0323ff946f7976987f41b3f7140808dcd1
Syed Salman Sami <syedsamigithub@gmail.com>
2026-08-03 22:31:01 +0600

    Add files via upload

---
+++ JS ES6 Fundamentals/nested_object.js
@@ -0,0 +1,48 @@
+const user = {
+    name: 'sami',
+    age: 26,
+    "my-address": "sheikhghat",
+    something: [20, 30, 40, 500],
+    company: {
+        name: "hieses",
+        netWorth: "500 Billion",
+        address: {
+            location: "sylhet",
+            street: "sheikhghat"
+        }
+    }
+}
+
+
+const user2 = {
+    name: 'sami',
+    age: 26,
+    "my-address": "sheikhghat",
+    something: [20, 30, 40, 500],
+    // company: {
+    //     name: "hieses",
+    //     netWorth: "500 Billion",
+    //     address: {
+    //         location: "sylhet",
+    //         street: "sheikhghat"
+    //     }
+    // }
+
+}
+
+// console.log(user.age)
+
+// const key = "company"
+// console.log(user[key])
+// console.log(user["my-address"])
+
+
+
+// console.log(user.company.name, user.company.address.street)
+// console.log(user2?.company?.name, user2?.company?.address?.street)
+
+
+// for(let key in user){
+//     console.log(key, user[key])
+// }
+
