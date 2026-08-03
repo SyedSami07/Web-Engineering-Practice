2a0483935f318cd4a2a32b324f5b61fbb78ea2a9
Syed Salman Sami <syedsamigithub@gmail.com>
2026-08-03 09:29:08 +0600

    Implement default parameter in calculateTotal function
    Added default parameter for tax in calculateTotal function.

--- JS ES6 Fundamentals/default_parameter.js
+++ JS ES6 Fundamentals/default_parameter.js
@@ -1,6 +1,17 @@
 function greet(name){
     return "hello, " + name;
 }
-console.log(greet("Sami"));
+// console.log(greet("Sami"));
 // console.log(greet());
 
+
+function makeCofee(suger){
+    console.log("making coffee with", suger, "spoons of sugar")
+}
+
+// console.log(makeCofee(2));
+
+function calculateTotal(price, tax = 50){
+    return price + tax;
+}
+console.log(calculateTotal(1000))
