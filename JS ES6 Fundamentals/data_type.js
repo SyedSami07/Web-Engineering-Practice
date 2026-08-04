d7e849583dfed92ea7424905e3a6e3c2b2ca1ca3
Syed Salman Sami <syedsamigithub@gmail.com>
2026-08-04 10:22:40 +0600

    Add files via upload

---
+++ JS ES6 Fundamentals/data_type.js
@@ -0,0 +1,35 @@
+// primitive 
+
+let name = "sami"
+let name2 = name;
+
+let age = 26
+let isMarried = false
+let universityResult = undefined
+let salary = null
+// console.log(typeof name, typeof age, typeof isMarried, typeof universityResult, typeof salary)
+
+// name = "syed"
+// name2 =" syed "
+// console.log(name2)
+
+
+// non-primitive
+
+let student = {
+    name: "sami"
+}
+
+let student2 =student
+student.age = 26
+// console.log(student2)
+
+function add(a,b){
+    return a+b}
+    // console.log(typeof student, typeof arr, typeof add)
+    
+let arr = [100,300,30]
+let arr2 = arr
+arr.push(5500)
+console.log(arr2)
+
