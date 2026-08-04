59d7743d40c897c5b34461ec7abbd02d97046294
Syed Salman Sami <syedsamigithub@gmail.com>
2026-08-04 16:31:13 +0600

    Add files via upload

---
+++ JS ES6 Fundamentals/scope_vs_hoisting.js
@@ -0,0 +1,36 @@
+// scope: three types
+// global scope
+// block scope
+// function scope
+
+// global scope
+let name = "sami"
+
+// if(true){
+//     console.log(name)
+// }
+
+
+
+// block scope
+
+// if(true){
+//     let age  = 24  
+// }
+//  console.log(age)
+
+
+
+// function scope
+
+
+// function add(a,b){
+//     let sum  = a+b
+//     return sum
+// }
+// console.log(add(5,4))
+
+{
+    var address = "sylhet"
+}
+console.log(address)
